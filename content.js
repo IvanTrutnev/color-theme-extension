f3c7893220089c98d74d58f4136fa5aab5243e81
Ivan Trutnev <ivan.trutneu@visata.org>
2020-03-17 16:11:46 +0300

    implement chrome storage changing logic

--- content.js
+++ content.js
@@ -1,4 +1,64 @@
 console.log('content');
+const STORAGE = {
+  color: '#a03',
+  range: 0.8
+};
+const wrapperId = 'screen-wrapper';
 
-chrome.runtime.sendMessage({color: 'f1b174'});
-localStorage.setItem('color', 'f1b174');
+class Main {
+  constructor() {
+    this.storage = STORAGE;
+    this.initStorage();
+    this.onStorageChanged();
+  }
+
+  initStorage() {
+    chrome.storage.local.get(this.storage, storage => {
+      this.storage = storage;
+      this.run();
+    });
+  }
+
+  saveStorage() {
+    chrome.storage.local.set(this.storage);
+  }
+
+  run() {
+    this.injectWrapper();
+  }
+
+  injectWrapper() {
+    const div = document.createElement('div');
+    div.id = wrapperId;
+    div.setAttribute('style', this.styles);
+    if (!document.getElementById(wrapperId)) {
+      document.documentElement.appendChild(div);
+    }
+  }
+
+  get styles() {
+    return `
+       z-index: 999999999;
+       background: ${this.storage.color}; 
+       pointer-events: none; 
+       position: fixed; 
+       top: 0%; 
+       right: 0%; 
+       width: 100%; 
+       height: 100%; 
+       opacity: ${(this.storage.range * 0.01 + 0.2).toFixed(4)};
+       mix-blend-mode: multiply; 
+     `;
+  }
+
+  onStorageChanged() {
+    chrome.storage.onChanged.addListener(changes => {
+      for (let key in changes) {
+        this.storage[key] = changes[key].newValue;
+      }
+      document.getElementById(wrapperId).setAttribute('style', this.styles);
+    });
+  }
+}
+
+const content = new Main();
--- popup.js
+++ popup.js
@@ -1,6 +1,10 @@
-// document.addEventListener('readystatechange', function () {
-//   const btn = document.getElementById('btn');
-//   const color = document.getElementById('color-theme');
-//   chrome.runtime.sendMessage({color: color.value});
-//   localStorage.setItem('color', color.value);
-// });
+document.addEventListener('DOMContentLoaded', function () {
+  const btn = document.getElementById('btn');
+  const color = document.getElementById('color-theme');
+  color.addEventListener('change', (e) => {
+    chrome.storage.local.set({
+      color: e.target.value,
+      range: 0.8
+    });
+  });
+});
