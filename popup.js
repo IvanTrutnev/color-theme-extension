ef00e71e46a80ecd8a615913ccc86cd50f6b843a
Ivan Trutnev <ivan.trutneu@visata.org>
2020-03-18 17:09:16 +0300

    add recommended color to popup

--- popup.js
+++ popup.js
@@ -1,10 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const btn = document.getElementById('btn');
-  const color = document.getElementById('color-theme');
-  color.addEventListener('change', (e) => {
-    chrome.storage.local.set({
-      color: e.target.value,
-      range: 0.8
-    });
+const saveColor = (color) => {
+  chrome.storage.local.set({
+    color,
+    range: 0.8
   });
+};
+document.addEventListener('DOMContentLoaded', function () {
+  const color = document.getElementById('color-theme');
+  const colorsWrapper = document.getElementById('colors-wrapper');
+
+  const colors = ['#fac563', '#ff8100', '#ff9329', '#ffa500', '#c5b791', '#90d6f2'];
+  for (let i = 0; i < colors.length; ++i) {
+    const item = document.createElement('div');
+    const colorItem = `<div>
+      <div class='color-item' style='background: ${colors[i]}' data-color=${colors[i]}></div>
+    </div>`;
+    item.innerHTML = colorItem;
+    colorsWrapper.appendChild(item);
+  }
+
+  chrome.storage.local.get(this.storage, storage => {
+    color.value = storage.color;
+  });
+
+  color.addEventListener('change', (e) => {
+    saveColor(e.target.value)
+  });
+  colorsWrapper.addEventListener('click', (e) => {
+    saveColor(e.target.dataset.color);
+  })
 });
